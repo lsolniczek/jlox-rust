2143bdf23f359ac1f01a91e2ceb0be47d0b91ebf
Lukasz Solniczek <lukasz.solniczek@allegro.pl>
2024-07-09 16:51:54 +0200

    add arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-def main() {
+use std::env;
+use std::path::PathBuf;
 
+fn main() {
+    let path_to_file: Option<String> = env::args().nth(1).or(None);
+
+    match path_to_file {
+        Some(path) => 
+            run_file(PathBuf::from(path)),
+        None => 
+            run_prompt()
+    }
+
+}
+
+fn run_file(file: PathBuf) {
+    dbg!(file);
+}
+
+fn run_prompt() {
+    dbg!(">  ");
 }
